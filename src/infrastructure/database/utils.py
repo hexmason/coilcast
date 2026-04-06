b99a32f5346f8606f8417e9a6271ca8e3d6e8942
hexmason <hexmason@proton.me>
2026-04-06 21:26:49 +0300

    Add session factory

--- src/infrastructure/database/utils.py
+++ src/infrastructure/database/utils.py
@@ -1,4 +1,4 @@
-from infrastructure.database.engine import engine
+from infrastructure.database.engine import engine, AsyncSessionLocal
 from infrastructure.database.models import *
 from infrastructure.database.models.base import Base
 
@@ -6,3 +6,8 @@ from infrastructure.database.models.base import Base
 async def init_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
